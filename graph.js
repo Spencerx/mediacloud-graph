5090daa54f239c12bfbe7d11cc90f609a0d958de
jdcc <justind.c.clark@gmail.com>
2012-10-15 14:25:57 -0400

    initial - graph is somewhat working

---
+++ graph.js
@@ -0,0 +1,80 @@
+var width = 1200,
+    height = 900;
+
+var force = d3.layout.force()
+    .size([width, height])
+    .charge(-400);
+
+var svg = d3.select("#graph").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json('graph.json', function(graph) {
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
+
+    var link = svg.selectAll("line.link")
+        .data(graph.links)
+        .enter()
+        .append("line")
+        .attr("class", "link")
+        .style("stroke-width", function(l) { return l.weight * 3; });
+
+    var node = svg.selectAll("circle.node")
+        .data(graph.nodes)
+        .enter()
+        .append("circle")
+        .attr("class", "node")
+        .attr("r", function(n) { return n.story_count * 4; })
+        .style("fill", function(n) { return d3.rgb(n.color.r, n.color.g, n.color.b); });
+
+    force.on("tick", function() {
+        var q = d3.geom.quadtree(graph.nodes),
+            i = 0,
+            n = node.length;
+
+        while (++i < n) {
+            q.visit(collide(node[i]));
+        }
+
+        link
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        node
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+    });
+});
+
+function collide(node) {
+  var r = node.radius + 16,
+      nx1 = node.x - r,
+      nx2 = node.x + r,
+      ny1 = node.y - r,
+      ny2 = node.y + r;
+  return function(quad, x1, y1, x2, y2) {
+    if (quad.point && (quad.point !== node)) {
+      var x = node.x - quad.point.x,
+          y = node.y - quad.point.y,
+          l = Math.sqrt(x * x + y * y),
+          r = node.radius + quad.point.radius;
+      if (l < r) {
+        l = (l - r) / l * .5;
+        node.x -= x *= l;
+        node.y -= y *= l;
+        quad.point.x += x;
+        quad.point.y += y;
+      }
+    }
+    return x1 > nx2
+        || x2 < nx1
+        || y1 > ny2
+        || y2 < ny1;
+  };
+}
+
---
+++ parse.rb
@@ -0,0 +1,63 @@
+require 'nokogiri'
+require 'json/ext'
+
+class MyDocument < Nokogiri::XML::SAX::Document
+    def start_document
+        @nodes = []
+        @links = []
+        @attributes = []
+    end
+
+    def end_document
+        # Change links from node IDs to node indexes
+        @links.map do |link|
+            link['source'] = @nodes.index { |node| node['id'] == link['source'] }
+            link['target'] = @nodes.index { |node| node['id'] == link['target'] }
+            link
+        end
+        File.open('graph.json', 'w').write({'nodes' => @nodes, 'links' => @links}.to_json)
+    end
+
+    def start_element name, attributes = []
+        attributes = Hash[attributes]
+        case name
+        when 'attribute'
+            @attributes << attributes
+        when 'edge'
+            @links << attributes.merge(attributes) { |key, value| value.to_i }
+        when 'node'
+            @active_node = {'id' => attributes['id'].to_i, 'label' => attributes['label']}
+        when 'attvalue'
+            attr = @attributes.select { |attr| attr['id'] == attributes['for'] }.first
+            key = attr['title']
+            case attr['type']
+            when 'integer'
+                @active_node[key] = attributes['value'].to_i
+            when 'float'
+                @active_node[key] = attributes['value'].to_f
+            else
+                @active_node[key] = attributes['value'] 
+            end
+        when 'spell'
+        when 'viz:color'
+            @active_node['color'] = {
+                'r' => attributes['r'].to_i, 
+                'g' => attributes['g'].to_i, 
+                'b' => attributes['b'].to_i
+            }
+        end
+    end
+
+    def end_element name
+        case name
+        when 'node'
+            @nodes << @active_node
+        end
+    end
+end
+
+# Create a new parser
+parser = Nokogiri::XML::SAX::Parser.new(MyDocument.new)
+
+# Feed the parser some XML
+parser.parse(File.open('sopa_media_link_monthly_2010-09-01_2010-10-02.gexf'))
