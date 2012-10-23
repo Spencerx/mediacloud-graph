6b0fe56417d8182a949fd2b5aae0b0fbb5be2d91
Justin Clark <jclark@cyber.law.harvard.edu>
2012-10-23 16:51:02 -0400

    good looking layout

--- graph.js
+++ graph.js
@@ -3,7 +3,10 @@ var width = 1200,
 
 var force = d3.layout.force()
     .size([width, height])
-    .charge(-500);
+    .gravity(0.01)
+    .charge(-80)
+    .linkDistance(function(d) { return (d.source.story_count + d.target.story_count) * 4 + 50; });
+
 
 var svg = d3.select("#graph").append("svg")
     .attr("width", width)
@@ -13,8 +16,14 @@ d3.json('graph.json', function(graph) {
     force
         .nodes(graph.nodes)
         .links(graph.links)
-        .start();
+    
+    nodes = force.nodes();
+    var n = nodes.length;
+    nodes.forEach(function(d, i) {
+      d.x = d.y = width / n * i;
+    });
 
+    force.start();
 
     var link = svg.selectAll("line.link")
         .data(graph.links)
@@ -28,16 +37,20 @@ d3.json('graph.json', function(graph) {
         .enter()
         .append("circle")
         .attr("class", "node")
-        .attr("r", function(n) { return 10; return n.story_count * 4; })
+        .attr("r", function(n) { return n.story_count * 4; })
         .style("fill", function(n) { return d3.rgb(n.color.r, n.color.g, n.color.b); });
+
+    node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
 
     force.on("tick", function() {
         var q = d3.geom.quadtree(graph.nodes),
             i = 0,
-            n = node.length;
+            n = nodes.length;
 
         while (++i < n) {
-            q.visit(collide(node[i]));
+            q.visit(collide(nodes[i]));
         }
 
         link
@@ -53,19 +66,20 @@ d3.json('graph.json', function(graph) {
 });
 
 function collide(node) {
-  var r = node.radius + 16,
+  var r = node.story_count * 4,
       nx1 = node.x - r,
       nx2 = node.x + r,
       ny1 = node.y - r,
       ny2 = node.y + r;
+
   return function(quad, x1, y1, x2, y2) {
     if (quad.point && (quad.point !== node)) {
       var x = node.x - quad.point.x,
           y = node.y - quad.point.y,
           l = Math.sqrt(x * x + y * y),
-          r = node.radius + quad.point.radius;
+          r = node.story_count * 4 + quad.point.story_count * 4;
       if (l < r) {
-        l = (l - r) / l * .5;
+        l = (l - r) / l * 0.003;
         node.x -= x *= l;
         node.y -= y *= l;
         quad.point.x += x;
