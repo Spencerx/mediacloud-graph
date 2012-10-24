fe77b223f54c163314e70611fe1bd787a3669fb9
Justin Clark <jclark@cyber.law.harvard.edu>
2012-10-24 17:54:12 -0400

    lots of changes

--- graph.js
+++ graph.js
@@ -1,14 +1,14 @@
 var width = 800,
     height = 800,
-    MAX_SIZE = 50,
-    MIN_SIZE = 1.5,
-    MAX_FONT_SIZE = 48,
-    MIN_FONT_SIZE = 8,
+    MAX_SIZE = 20,
+    MIN_SIZE = 2,
+    MAX_FONT_SIZE = 36,
+    MIN_FONT_SIZE = 3,
     X_MARGIN = 60,
     Y_MARGIN = 60,
-    TIMEOUT = 3000,
+    TIMEOUT = 2000,
     DURATION = 1500,
-    LABEL_CUTOFF = 16;
+    LABEL_CUTOFF = 4;
 
 var svg = d3.select("#graph").append("svg")
     .attr("width", width)
@@ -17,13 +17,18 @@ var svg = d3.select("#graph").append("svg")
 var radius = d3.scale.linear().domain([0,1]).range([MIN_SIZE, MAX_SIZE]);
 var fontSize = d3.scale.linear().domain([LABEL_CUTOFF,MAX_SIZE]).range([MIN_FONT_SIZE, MAX_FONT_SIZE]).clamp(true);
 
-d3.json('frames2.json', function(frames) {
-    frames.forEach(function(frame, i) {
-        setTimeout(go, i * TIMEOUT, frame);
-    });
-});
+d3.select('#date-slider').style('width', (width - 2 * X_MARGIN) + 'px');
+d3.select('#graph-wrapper').style('width', width + 'px');
 
-function go(frame) {
+function play(frames) {
+    frames.forEach(function(frame, i) {
+        setTimeout(go, i * TIMEOUT, frame, i);
+    })
+}
+
+function go(frame, i) {
+    // Keep slider in sync with playing
+    if (typeof i != 'undefined') { $('#date-slider').slider('value', i); }
     var nodes = svg.selectAll("g.node")
         .data(frame.nodes, function(n) { return n.id; })
 
@@ -31,7 +36,14 @@ function go(frame) {
     var group = nodes.enter()
         .append("g")
         .attr("class", "node")
-        .attr("transform", function(n) { return "translate(" + convertXCoord(n.position.x) + "," + convertYCoord(n.position.y) + ")"; });
+        .attr("transform", function(n) {
+            return "translate("
+                + convertXCoord(n.position.x)
+                + ","
+                + convertYCoord(n.position.y)
+                + ")";
+        })
+        .classed('not-selected', function() { return !d3.select('.not-selected').empty(); })
 
     group
         .append('circle')
@@ -56,21 +68,56 @@ function go(frame) {
         .select('circle')
         //.ease('linear')
         .attr("r", function(n) { return radius(n.size); })
-        .style("fill", function(n) { return d3.rgb(n.color.r, n.color.g, n.color.b); });
+        .style("fill", function(n) { return d3.rgb(n.color.r, n.color.g, n.color.b); })
     
     trans
         .select('text')
         .style('font-size', function(n) {
             var size = fontSize(radius(n.size));
             return size > LABEL_CUTOFF ? size + 'px' : '0';
-        });
+        })
 
     // Exit
-    nodes.exit()
-        .transition()
+    var exit = nodes.exit(),
+        exitTrans = exit.transition();
+
+    exitTrans
         .select('circle')
-        .attr('r', 0)
-        .remove();
+        .attr('r', 0);
+
+    exitTrans
+        .select('text')
+        .style('font-size', '0px');
+
+    exitTrans.remove();
+
+    // Keep node-info in sync with data in node
+    userSelected = svg.select('g.node.selected');
+    if (!userSelected.empty()) {
+        populateNodeInfo(userSelected.data()[0]);
+    }
+
+    // If selected nodes are removed, make everything normal again
+    if (!exit.filter('.selected').empty()) {
+        nodes.classed('not-selected', false).classed('selected', false);
+        d3.select('#node-info').html('');
+    }
+    
+    // Event handlers
+    nodes.on('click', function() {
+        var node = d3.select(this).data()[0];
+        d3.select(this).classed('selected', true);
+        nodes.classed('not-selected', function(n) { return n != node; });
+        populateNodeInfo(node);
+    });
+
+    svg.on('click',
+        function() {
+            nodes.classed('not-selected', false).classed('selected', false);
+            d3.select('#node-info').html('');
+        },
+    // Capture
+    true);
 
     /*
     var links = svg.selectAll("line.link")
@@ -107,3 +154,26 @@ function convertXCoord(x) {
 function convertYCoord(y) {
     return height - y * (height - Y_MARGIN * 2) - Y_MARGIN;
 }
+
+function populateNodeInfo(node) {
+    d3.select('#node-info').html(
+        Object.keys(node).map(
+            function(key) {
+                return '<dt>' + key + '</dt><dd>' + node[key] + '</dd>';
+            }
+        ).reduce(function(prev, curr) { return prev + curr; })
+    );
+}
+
+d3.json('frames3.json', function(frames) {
+    var slider = $('#date-slider').slider({
+        min: 0,
+        max: frames.length - 1,
+        range: "min",
+        change: function(event, ui) {
+            go(frames[ui.value]);
+        }
+    });
+    go(frames[0]);
+    d3.select('#play').on('click', function(d, i) { play(frames); });
+});
--- parse.rb
+++ parse.rb
@@ -68,13 +68,13 @@ end
 # Create a new parser
 parser = Nokogiri::XML::SAX::Parser.new(MyDocument.new)
 
-filenames = Dir.entries('key_frames2').reject! { |filename| filename[0] == '.' }.sort_by! { |filename| filename.to_i }
+filenames = Dir.entries('key_frames3').reject! { |filename| filename[0] == '.' }.sort_by! { |filename| filename.to_i }
 
 # Feed the parser some XML
 #parser.parse(File.open('data/sopa_media_link_monthly_2010-10-02_2010-11-02.gexf'))
 filenames.each do |filename|
-    puts "key_frames2/#{filename}"
-    parser.parse_file("key_frames2/#{filename}")
+    puts "key_frames3/#{filename}"
+    parser.parse_file("key_frames3/#{filename}")
 end
 
-File.open('frames2.json', 'w') {|f| f.write(@@frames.to_json) }
+File.open('frames3.json', 'w') {|f| f.write(@@frames.to_json) }
